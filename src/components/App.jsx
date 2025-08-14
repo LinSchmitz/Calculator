fe1b98cd5d3238534a261ad3fa5e138f515139fb
naleilan <leilanschmitz@gmail.com>
2025-08-14 20:17:56 +0330

    added template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
 function App() {
-  return <div>app</div>;
+  return (
+    <div className="container">
+      <div className="output">
+        <div className="span-2">AC</div>
+        <div>DEL</div>
+      </div>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-2">=</button>
+    </div>
+  );
 }
 
 export default App;
